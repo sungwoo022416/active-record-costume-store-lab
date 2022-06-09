daf368f22975f8c60b1a4705c5eb4d1027dbcdd8
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-06-09 12:39:49 -0700

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,5 @@
 # Create your Costume class here
+class Costume < ActiveRecord::Base
+    #attr_reader :name, :price, :image_url, :size, :created_at, :updated_at
+end
 # It should inherit from ActiveRecord::Base
---
+++ db/migrate/tempCodeRunnerFile.rb
@@ -0,0 +1,1 @@
+l out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
